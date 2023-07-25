8cb5792c84c0bf54cec2c9d0984ed2902d08d80f
Lucas-SJY <Lucas_SJY@outlook.com>
2023-07-25 15:24:40 +0800

    fast power

---
+++ fast_power.py
@@ -0,0 +1,12 @@
+def fast_power(a, n):
+    if n == 0:
+        return 1
+    if n == 1:
+        return a
+    if n % 2 == 1:
+        return fast_power(a, n - 1) * a
+    else:
+        return fast_power(a, n / 2) * fast_power(a, n / 2)
+
+
+print(fast_power(2, 10000))
